1e72bd2ed3ec30e8a65651a18b2890d28a036464
Matthew Booe <mabooe@microsoft.com>
2021-10-11 21:00:09 +0000

    WIP on getting "Hello There" working
    Co-authored-by: Joyce Er <joyce.er@microsoft.com>

---
+++ DESIGN.md
@@ -0,0 +1,133 @@
+# Design of the Force
+
+- False
+    - Amazing, every word of what you just said is wrong
+    - Oh, not good
+    - Bad motivator
+    - **No, that's not true. That's impossible**
+    - We don’t serve there kind here
+    - That’s no moon
+- True
+    - What I told you was true from **a certain point of view**
+    - Help me Obi-Wan Kenobi, you’re my only hope
+    - Search you feelings lord Vader, you know it to be true
+- If
+    - Try not. Do or do not. There is no try.
+    - **Do or do not.**
+- Else
+    - These aren’t the droids you’re looking for
+- EndIf
+    - **You have failed me for the last time**
+    - Now his failure is complete
+- While
+    - I'll try spinning, that's a good trick
+    - **Here we go again**
+- EndWhile
+    - Let the past die
+- For
+    - For a thousand generations the Jedi were guardians of the republic
+- EndFor
+    - **You cannot escape your destiny**
+    - When 900 years old you reach, look as good, you will not
+- +
+    - This will make a fine addition to my collection
+- -
+    - We lost something
+    - **Proceed with the countdown**
+- *
+    - There's too many of them
+- ^
+    - Unlimited power
+- /
+    - Not to worry, at least we are flying half a ship
+- %
+    - Never tell me the odds
+- ==
+    - You're a Jedi too, nice to meet you
+- >
+    - There is always a bigger fish
+- <
+    - Impressive. Most impressive
+- Or
+    - Never tell me the odds
+- And
+    - There is another
+- Not
+    - Always with you what cannot be done
+- Noop
+    - Move along. Move along
+- DeclareMethod
+    - What is thy bidding, my master
+    - **This is where the fun begins**
+- NonVoidMethod
+    - All right that's pretty much nowhere
+- MethodArguments
+    - Now that’s a name I have not heard in a long time, a long time
+- Return
+    - There is nothing for me here now
+    - Let the Wookiee win
+    - **You’re all clear kid, let's blow this thing and go home**
+- EndMethodDeclaration
+    - **It is clear to me the Republic no longer functions**
+    - You want to go home and rethink your life
+    - You know what you need to do
+- CallMethod
+    - I have a bad feeling about this
+    - **I hope you know what you’re doing**
+- AssignVariableFromMethodCall
+    - You saved me
+    - As you wish
+    - **Many Bothans died to bring us this information**
+- DeclareInt
+    - Yoda, you seek Yoda
+- DeclareString
+    - 
+- DeclareBool
+    - I am the senate
+- SetInitialValue
+    - The maker!
+    - Whoosa are youssa
+- BeginMain
+    - **Do it**
+    - This is where the fun begins
+- EndMain
+    - **May The Force be with you**
+- Print
+    - Use the Force
+    - What is meessa saying
+    - **The Sacred Texts!**
+- ReadInteger
+    - Looking, found someone I would say you have
+- ReadString
+    - 
+- AssignVariable
+    - What a piece of junk
+- SetValue
+    - I am your father
+    - I am a Jedi, like my father before me
+- EndAssignVariable
+    - The garbage will do
+- ParseError
+    - Sometimes there are things no one can fix.
+    - I trusted them to fix it. It's not my fault
+    - Don't blame me, I'm an interpreter
+    - I’m not much more than an interpreter
+    - You will find it is you that are mistaken about a great many things
+    - I felt a great disturbance in the Force
+    - I’m standing here in pieces and you’re having dillusions of grandeur
+    - Your feeble skills are no match for the power of the dark side
+    - You want to go home and rethink your life
+    - That's not how the Force works
+    - I like where your head's at but no
+    - Failure, the greatest teacher is
+    - How rude
+    - Will this agony ever end?
+    - Amazing, every word of what you just said is wrong
+    
+
+Thinhs we must use
+- It's a trap
+- I am the senate
+- I’ll try spinning, thats a good trick
+- Now this is pod racing
+- Size matters not
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,95 @@
-use inkwell::context::Context;
-use inkwell::execution_engine::JitFunction;
-use inkwell::OptimizationLevel;
+// use inkwell::context::Context;
+// use inkwell::execution_engine::JitFunction;
+// use inkwell::OptimizationLevel;
+extern crate pest;
+extern crate pest_derive;
+
+use pest::Parser;
+
+pub enum Node {
+    // Int(i32),
+    String(String),
+    // Op1(String, Expression),
+    // Op2(String, Expression, Expression),
+    // Function(String, Vec<Expression>),
+    BeginMain,
+    EndMain,
+    Print(Box<Node>)
+}
+
+#[derive(pest_derive::Parser)]
+#[grammar = "grammar.pest"]
+struct ForceParser;
+
+fn parse(source: &str) -> Result<Vec<Node>, pest::error::Error<Rule>> {
+    let mut ast = vec![];
+    let pairs = ForceParser::parse(Rule::Program, source)?;
+    for pair in pairs {
+        if let Rule::Expr = pair.as_rule() {
+            ast.push(build_ast_from_expr(pair));
+        }
+    }
+    Ok(ast)
+}
+
+fn build_ast_from_expr(pair: pest::iterators::Pair<Rule>) -> Node {
+    match pair.as_rule() {
+        // Rule::Expr => build_ast_from_expr(pair.into_inner().next().unwrap()),
+        // Rule::UnaryExpr => {
+        //     let mut pair = pair.into_inner();
+        //     let op = pair.next().unwrap();
+        //     let child = pair.next().unwrap();
+        //     let child = build_ast_from_term(child);
+        //     parse_unary_expr(op, child)
+        // }
+        // Rule::BinaryExpr => {
+        //     let mut pair = pair.into_inner();
+        //     let lhspair = pair.next().unwrap();
+        //     let lhs = build_ast_from_term(lhspair);
+        //     let op = pair.next().unwrap();
+        //     let rhspair = pair.next().unwrap();
+        //     let rhs = build_ast_from_term(rhspair);
+        //     parse_binary_expr(op, lhs, rhs)
+        // }
+        Rule::Main => {
+            let mut pair = pair.into_inner();
+            let begin_main = pair.next().unwrap();
+            let expr = pair.next().unwrap();
+            let end_main = pair.next().unwrap();
+            build_ast_from_expr(expr)
+        }
+        Rule::Expr => {
+            let mut pair = pair.into_inner();
+            let print = pair.next().unwrap();
+            let string = pair.next().unwrap();
+            Node::Print(Box::new(Node::String(string.to_string())))
+        }
+        unknown => panic!("Unknown expr: {:?}", unknown),
+    }
+}
+
+fn evaluate(exp: Node) {
+    match exp {
+        Node::String(x) => println!("{}", x),
+    }
+}
+
+fn main() {
+    let exp = Node::String("Hello there".to_string());
+    evaluate(exp);
+}
+
+/* pub struct String {
+    content: Vec<i8>
+}
+
+impl String {
+    fn new() -> String {
+        String {
+            content: Vec::new()
+        }
+    } 
+}
 
 fn main() {
     // ANCHOR: first
@@ -8,6 +97,7 @@ fn main() {
     let module = context.create_module("addition");
     let i32_type = context.i32_type();
     // ANCHOR_END: first
+
     // ANCHOR: second
     let fn_type = i32_type.fn_type(&[i32_type.into(), i32_type.into()], false);
     let fn_val = module.add_function("add", fn_type, None);
@@ -16,6 +106,7 @@ fn main() {
     let builder = context.create_builder();
     builder.position_at_end(entry_basic_block);
     // ANCHOR_END: second
+
     // ANCHOR: third
     let x = fn_val.get_nth_param(0).unwrap().into_int_value();
     let y = fn_val.get_nth_param(1).unwrap().into_int_value();
@@ -23,9 +114,11 @@ fn main() {
     let ret = builder.build_int_add(x, y, "add");
     let return_instruction = builder.build_return(Some(&ret));
     // ANCHOR_END: third
+
     dbg!("module: {:?}", module.clone());
     dbg!("builder: {:?}", builder);
     assert_eq!(return_instruction.get_num_operands(), 1);
+    
     // ANCHOR: fourth
     let execution_engine = module
         .create_jit_execution_engine(OptimizationLevel::None)
@@ -39,3 +132,4 @@ fn main() {
     }
     // ANCHOR_END: fourth
 }
+ */
