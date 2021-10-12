cc14329a79de20a42cb57b4fcfe9fc4ccb339757
Matthew Booe <mirdaki@users.noreply.github.com>
2021-10-12 09:13:31 -0700

    Add a hardcoded Hello there (#1)
    * WIP on getting "Hello There" working
    
    Co-authored-by: Joyce Er <joyce.er@microsoft.com>
    
    * Get Hello There working

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,51 @@
-use inkwell::context::Context;
-use inkwell::execution_engine::JitFunction;
-use inkwell::OptimizationLevel;
+extern crate pest;
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Debug)]
+pub enum Node {
+    Print(String)
+}
+
+#[derive(pest_derive::Parser)]
+#[grammar = "grammar.pest"]
+struct ForceParser;
+
+fn parse(source: &str) -> Result<Vec<Node>, pest::error::Error<Rule>> {
+    let mut ast = vec![];
+    let pairs = ForceParser::parse(Rule::Program, source)?;
+    for pair in pairs {
+        if let Rule::Main = pair.as_rule() {
+            ast.push(build_ast(pair));
+        }
+    }
+    Ok(ast)
+}
+
+fn build_ast(pair: pest::iterators::Pair<Rule>) -> Node {
+    match pair.as_rule() {
+        Rule::Main => {
+            let mut pair = pair.into_inner();
+            let string = pair.next().unwrap();
+            Node::Print(string.as_span().as_str().to_string())
+        }
+        unknown => panic!("Unknown expr: {:?}", unknown),
+    }
+}
+
+fn evaluate(ast: &Node) {
+    match ast {
+        Node::Print(x) => println!("{}", x),
+    }
+}
 
 fn main() {
-    // ANCHOR: first
-    let context = Context::create();
-    let module = context.create_module("addition");
-    let i32_type = context.i32_type();
-    // ANCHOR_END: first
-    // ANCHOR: second
-    let fn_type = i32_type.fn_type(&[i32_type.into(), i32_type.into()], false);
-    let fn_val = module.add_function("add", fn_type, None);
-    let entry_basic_block = context.append_basic_block(fn_val, "entry");
-
-    let builder = context.create_builder();
-    builder.position_at_end(entry_basic_block);
-    // ANCHOR_END: second
-    // ANCHOR: third
-    let x = fn_val.get_nth_param(0).unwrap().into_int_value();
-    let y = fn_val.get_nth_param(1).unwrap().into_int_value();
-
-    let ret = builder.build_int_add(x, y, "add");
-    let return_instruction = builder.build_return(Some(&ret));
-    // ANCHOR_END: third
-    dbg!("module: {:?}", module.clone());
-    dbg!("builder: {:?}", builder);
-    assert_eq!(return_instruction.get_num_operands(), 1);
-    // ANCHOR: fourth
-    let execution_engine = module
-        .create_jit_execution_engine(OptimizationLevel::None)
-        .unwrap();
-    unsafe {
-        type Addition = unsafe extern "C" fn(i32, i32) -> i32;
-        let add: JitFunction<Addition> = execution_engine.get_function("add").unwrap();
-        let x = 1;
-        let y = 2;
-        assert_eq!(add.call(x, y), x + y);
-    }
-    // ANCHOR_END: fourth
+    let source = r#"
+    Do it
+        The Sacred Texts! "Hello there"
+    May The Force be with you
+    "#;
+    let ast = parse(source);
+    evaluate(ast.unwrap().first().unwrap());
 }
