770964fb27ef1efe58de1c7ed7ced7493c5d3949
Matthew Booe <mabooe@microsoft.com>
2021-10-14 00:14:18 +0000

    WIP towards LLVM support
    Co-authored-by: Jake Bailey <jakebailey@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 mod ast;
 mod parser;
 mod interpreter;
+mod compiler;
 
 fn main() {
     let source = r#"
-Do it!
-The Sacred Texts! "Hello there"
-May The Force be with you.
+Do it
+    The Sacred Texts! "Hello there"
+May The Force be with you
     "#;
     let ast = parser::parse(source);
     interpreter::evaluate(ast.unwrap().first().unwrap());
