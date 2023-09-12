2a77c12186f1cf29cdea0365e01e9f00c53ad766
Forgind <12969783+Forgind@users.noreply.github.com>
2023-09-12 11:20:17 -0700

    Revert CLI change

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,8 @@ fn main() -> Result<(), String> {
 
     let ast = parser::parse(source.as_str());
 
-    if args.contains_id("parse_error") {
-        match ast {
-            Ok(t) => interpreter::evaluate(t, io::stdin().lock(), io::stdout()),
-            Err(e) => Err(format!("Error encountered while parsing: {}", e).to_string()),
-        }
-    }
-    else {
-        interpreter::evaluate(ast.unwrap(), io::stdin().lock(), io::stdout())
+    match ast {
+        Ok(t) => interpreter::evaluate(t, io::stdin().lock(), io::stdout()),
+        Err(e) => Err(format!("Error encountered while parsing: {}", e).to_string()),
     }
 }
